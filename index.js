c76201186c4728bc50a3908d4eaa5ee61d227ff5
Pavel.K <pavelweb74@gmail.com>
2021-12-24 22:05:27 +0500

    Add server listener

--- index.js
+++ index.js
@@ -3,4 +3,9 @@ const http = require("http");
 
 config();
 
-const server = http.createServer((req, res) => {});
+const server = http.createServer((req, res) => {
+  res.end("Server response");
+});
+server.listen(process.env.PORT, () => {
+  console.log(`Server started on port ${process.env.PORT}`);
+});
