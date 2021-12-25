509c727dbc317a035563fa53e778ea5d02064451
Pavel.K <pavelweb74@gmail.com>
2021-12-25 20:08:38 +0500

    Work with routes

--- index.js
+++ index.js
@@ -1,18 +1,60 @@
 const config = require("dotenv").config;
+config();
+
 const http = require("http");
 const EventEmitter = require("events");
 
-config();
-
 const emitter = new EventEmitter();
 
-const server = http.createServer((req, res) => {
-  emitter.emit(`[${req.url}]:[${req.method}]`);
+class Router {
+  constructor() {
+    this.endpoints = {};
+    emitter.on("test", () => {});
+  }
+  request(method = "GET", path, handler) {
+    if (!this.endpoints[path]) {
+      this.endpoints[path] = {};
+    }
+    const endpoint = this.endpoints[path];
+    if (endpoint[method]) {
+      throw new Error("Handler alreary exists");
+    }
+    endpoint[method] = handler;
+    emitter.on(`[${path}]:[${method}]`, (req, res) => {
+      handler(req, res);
+    });
+  }
+  get(path, handler) {
+    this.request("GET", path, handler);
+  }
+  post(path, handler) {
+    this.request("POST", path, handler);
+  }
+  path(path, handler) {
+    this.request("PATH", path, handler);
+  }
+  put(path, handler) {
+    this.request("PUT", path, handler);
+  }
+  delete(path, handler) {
+    this.request("DELETE", path, handler);
+  }
+}
 
-  res.writeHead(200, {
-    "Content-type": "text/html;charset=utf-8",
-  });
-  res.end("Server response");
+const router = new Router();
+
+router.get("/users", (req, res) => {
+  res.end("users");
+});
+router.get("/posts", (req, res) => {
+  res.end("posts");
+});
+
+const server = http.createServer((req, res) => {
+  const emitted = emitter.emit(`[${req.url}]:[${req.method}]`, req, res);
+  if (!emitted) {
+    res.end("Server response");
+  }
 });
 server.listen(process.env.PORT, () => {
   console.log(`Server started on port ${process.env.PORT}`);
