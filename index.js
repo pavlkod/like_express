af102017a7b559c67f29e362f25f21013c87ea86
Pavel.K <pavelweb74@gmail.com>
2021-12-25 15:26:42 +0500

    Add routes

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const config = require("dotenv").config;
 const http = require("http");
+const Router = require("./Router");
 
 config();
+
+const router = new Router();
+
+router.get("/users", (req, res) => {
+  res.end("users");
+  console.log("users");
+});
+router.get("/posts", (req, res) => {
+  res.end("posts");
+  console.log("posts");
+});
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
