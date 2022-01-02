01eb88d3089589bdeb3320a9bb4b3210d087f8b1
Pavel.K <pavelweb74@gmail.com>
2022-01-02 16:41:41 +0500

    Add parseUrl

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const config = require("dotenv").config;
 const Application = require("./framework/Application");
 const router = require("./router");
 const jsonParser = require("./framework/parseJson");
+const parseUrl = require("./framework/parseUrl");
 
 config();
 
 const app = new Application();
 
 app.use(jsonParser);
+app.use(parseUrl);
 app.addRoute(router);
 
 app.listen(process.env.PORT, () => {
