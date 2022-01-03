d37be82438e11371687d8d7f6ad8452db1769d28
Pavel.K <pavelweb74@gmail.com>
2022-01-03 15:23:02 +0500

    Add find user by id

--- router/index.js
+++ router/index.js
@@ -7,6 +7,9 @@ const users = [
 ];
 
 router.get("/users", (req, res) => {
+  if (req.params.id) {
+    return res.send(users.find(user => user.id === +req.params.id));
+  }
   res.send(users);
 });
 router.post("/users", (req, res) => {
