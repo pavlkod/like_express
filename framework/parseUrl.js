02a1a9c07727e1b5fad8ac26e082c2bb15b55a43
Pavel.K <pavelweb74@gmail.com>
2022-01-04 13:48:48 +0500

    Add req.params in middleware

--- framework/parseUrl.js
+++ framework/parseUrl.js
@@ -1,4 +1,7 @@
 module.exports = baseURL => (req, res) => {
   const parsedUrl = new URL(req.url, baseURL);
+  const params = {};
+  parsedUrl.searchParams.forEach((value, key) => (params[key] = value));
+  req.params = params;
   req.pathname = parsedUrl.pathname;
 };
