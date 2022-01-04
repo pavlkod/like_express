d9f8c734f5ca483b6c4da590c2688384a6727f62
Pavel.K <pavelweb74@gmail.com>
2022-01-04 13:59:46 +0500

    Use getUsers controller in router

--- router/index.js
+++ router/index.js
@@ -1,17 +1,9 @@
+const RouterController = require("../controllers/routerController");
+
 const Router = require("../framework/Router");
 const router = new Router();
 
-const users = [
-  { id: 1, name: "Test" },
-  { id: 2, name: "Test2" },
-];
-
-router.get("/users", (req, res) => {
-  if (req.params.id) {
-    return res.send(users.find(user => user.id === +req.params.id));
-  }
-  res.send(users);
-});
+router.get("/users", RouterController.getUsers);
 router.post("/users", (req, res) => {
   const body = req.body;
   users.push(body);
