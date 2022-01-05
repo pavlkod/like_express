3b43d975bf1059db1176987bb10b7278028d626e
Pavel.K <pavelweb74@gmail.com>
2022-01-05 19:17:45 +0500

    Add use User model in getUsers controller

--- controllers/routerController.js
+++ controllers/routerController.js
@@ -2,15 +2,18 @@ const users = [
   { id: 1, name: "Test" },
   { id: 2, name: "Test2" },
 ];
-
+const User = require("../models/user");
 class RouterController {
-  getUsers(req, res) {
+  async getUsers(req, res) {
+    let users;
     if (req.params.id) {
-      return res.send(users.find(user => user.id === +req.params.id));
+      users = await User.findById(req.params.id);
+      return res.send(users);
     }
+    users = await User.find();
     res.send(users);
   }
-  createUser(req, res) {
+  async createUser(req, res) {
     const body = req.body;
     users.push(body);
     res.send(body);
